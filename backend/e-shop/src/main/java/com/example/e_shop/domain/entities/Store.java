dd89dde1c7a32482b6c2964573d9acc0527c4479
SunHuy <nghuy1115@gmail.com>
2025-05-06 23:49:34 +0700

    Revert "Adding more domain entities"
    This reverts commit 8c1dccf09e558ec16708a413ceddfea2a19c2dbc.

--- backend/e-shop/src/main/java/com/example/e_shop/domain/entities/Store.java
+++ backend/e-shop/src/main/java/com/example/e_shop/domain/entities/Store.java
@@ -1,16 +1,4 @@
 package com.example.e_shop.domain.entities;
 
-import com.example.e_shop.domain.valueObjects.Address;
-import com.example.e_shop.domain.valueObjects.Phone;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class Store {
-    private final Long id;
-    private final User user;
-    private String name;
-    private Address address;
-    private Phone phone;
 }
--- backend/e-shop/src/main/java/com/example/e_shop/domain/valueObjects/Email.java
+++ backend/e-shop/src/main/java/com/example/e_shop/domain/valueObjects/Email.java
@@ -1,37 +1,4 @@
 package com.example.e_shop.domain.valueObjects;
 
-import lombok.Getter;
-
-import java.util.Objects;
-import java.util.regex.Pattern;
-
-@Getter
-public final class Email {
-    private static final String EMAIL_REGEX =
-            "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
-
-    private final String value;
-
-    public Email(String value) {
-        if (value == null || !EMAIL_PATTERN.matcher(value).matches()) throw new IllegalArgumentException("Invalid email address: " + value);
-        this.value = value.toLowerCase(); // normalize
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Email email)) return false;
-        return value.equals(email.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
+public class Email {
 }
